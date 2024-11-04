6af9e134b963e01ad6c396fc9154ea6dde3bdd8a
Ceprnic <ceprnic.josip@gmail.com>
2024-11-04 17:01:49 +0100

    implement game over scene

--- Bird/Assets/BirdScript.cs
+++ Bird/Assets/BirdScript.cs
@@ -7,6 +7,8 @@ public class BirdScript : MonoBehaviour
 
     public Rigidbody2D birdRigidbody;
     public float goUpForce = 5;
+    public GameLogicScript logic;
+    public bool isAlive = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,8 @@ public class BirdScript : MonoBehaviour
             birdRigidbody = GetComponent<Rigidbody2D>();
         }
 
+        logic = GameObject.FindGameObjectWithTag("logic").GetComponent<GameLogicScript>();
+
 
 
     }
@@ -23,11 +27,18 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
             birdRigidbody.velocity = Vector2.up * goUpForce;
 
         }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameFinished();
+        isAlive = false;
 
     }
 }
--- Bird/Assets/GameLogicScript.cs
+++ Bird/Assets/GameLogicScript.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameLogicScript : MonoBehaviour
 {
     public int score = 0;
     public Text scoreText;
+    public GameObject gameOver;
 
     [ContextMenu("Increase Score")]
     public void addScore()
     {
         score++;
         scoreText.text = score.ToString();
+    }
+
+    public void restartFlappy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameFinished()
+    {
+        gameOver.SetActive(true);
     }
 
 }
